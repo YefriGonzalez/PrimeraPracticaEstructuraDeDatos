4e08a143148265cd83b15e5ed4b2a4f64994ae53
Yefri González <yefrig00@gmail.com>
2022-03-02 22:03:46 -0600

    Creacion Practica 1 Estructura de datos

---
+++ src/main/java/com/yefrig/practica1estructuradedatos/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yefrig.practica1estructuradedatos;
+
+/**
+ *
+ * @author yefri
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
